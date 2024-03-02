5b70ae0a596735bd64c0bac737602e8568ba645c
AhmedRazaaAttari <154536166+AhmedRazaaAttari@users.noreply.github.com>
2024-03-02 13:32:33 +0500

    Add files via upload

---
+++ Alert-chapter.No.02.JS/app.js
@@ -0,0 +1,43 @@
+// chapter-NO-2
+// 1. Declare a variable called username.
+
+    alert("Ahmed raza")
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your age.
+
+    alert("Ahmed Raza Attari\n21 year old")
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+    alert("Hello world!")
+
+// Write a script to display the following alert using one JS
+// variable:
+
+    alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+    alert("e.g. ahmedraza575557@gmail.com")
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+    alert("I am trying to learn from the book A smarter\n way to learn JavaScript.")
+
+// 8. Write a script to display this in browser through JS
+
+    document.write("Yah! I can write HTML contect through javascript")
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+
+    alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
